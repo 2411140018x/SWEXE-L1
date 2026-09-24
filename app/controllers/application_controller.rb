2edf92cc918b20e98f5c6f171fb553c50c5b0bdf
2411140018x <2411140018x@ed.fuk.kindai.ac.jp>
2026-09-24 16:52:54 +0000

    新規登録ができない問題を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,15 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+
+  private
+
+  def valid_request_origin?
+    super || (
+      Rails.env.development? &&
+      request.origin == "https://localhost:3000" &&
+      request.host.end_with?(".app.github.dev")
+    )
+  end
 end
