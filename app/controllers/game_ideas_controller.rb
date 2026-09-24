7230e50440ce831f7cee675a552eb0f346c56766
2411140018x <2411140018x@ed.fuk.kindai.ac.jp>
2026-09-24 16:06:25 +0000

    新規登録画面作成

--- app/controllers/game_ideas_controller.rb
+++ app/controllers/game_ideas_controller.rb
@@ -2,4 +2,24 @@ class GameIdeasController < ApplicationController
   def index
     @game_ideas = GameIdea.order(created_at: :desc)
   end
+
+  def new
+    @game_idea = GameIdea.new
+  end
+
+  def create
+    @game_idea = GameIdea.new(game_idea_params)
+
+    if @game_idea.save
+      redirect_to game_ideas_path, notice: "ゲームアイデアを登録しました。"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def game_idea_params
+    params.require(:game_idea).permit(:title, :genre, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :game_ideas, only: [ :index ]
+  resources :game_ideas, only: [ :index, :new, :create ]
   root "game_ideas#index"
 end
